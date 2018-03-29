9886c0b4957931de51bc1a61bf968b500545e14e
guhao <18810443670@163.com>
2018-03-29 15:16:04 +0800

    config

--- config.php
+++ config.php
@@ -1,14 +1,28 @@
 <?php
-// HTTP
-define('HTTP_SERVER', 'http://mall.szyimafu.com/');
 
-// HTTPS
-define('HTTPS_SERVER', 'http://mall.szyimafu.com/');
 
-// DIR
-define('DIR_APPLICATION', '/home/wwwroot/shop/catalog/');
-define('DIR_SYSTEM', '/home/wwwroot/shop/system/');
-define('DIR_IMAGE', '/home/wwwroot/shop/image/');
+//判断是线上还是测试
+if (strpos($_SERVER['REMOTE_ADDR'], '192.168') !== false) {
+    // HTTP
+    define('HTTP_SERVER', 'http://shop.codepay.cc/');
+    // HTTPS
+    define('HTTPS_SERVER', 'http://shop.codepay.cc/');
+    // DIR
+    define('DIR_APPLICATION', '/home/web/shop/catalog/');
+    define('DIR_SYSTEM', '/home/web/shop/system/');
+    define('DIR_IMAGE', '/home/web/shop/image/');
+}else{
+    // HTTP
+    define('HTTP_SERVER', 'http://mall.szyimafu.com/');
+    // HTTPS
+    define('HTTPS_SERVER', 'http://mall.szyimafu.com/');
+    // DIR
+    define('DIR_APPLICATION', '/home/wwwroot/shop/catalog/');
+    define('DIR_SYSTEM', '/home/wwwroot/shop/system/');
+    define('DIR_IMAGE', '/home/wwwroot/shop/image/');
+}
+
+
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
@@ -39,6 +53,6 @@ define('GOLD_PAYURL', 'https://api.goldpayment.net/pay/');//支付请求的url
 
 define('GOLD_KEY', 'UVIXnJb7W4kpFevdDum5MHNBZSj06q39');//商户秘钥
 
-define('GOLD_NOTIFY', HTTP_SERVER.'index.php?route=payment/notify');//支付回调地址
+define('GOLD_NOTIFY', HTTP_SERVER . 'index.php?route=payment/notify');//支付回调地址
 
-define('GOLD_NOTICE', HTTP_SERVER.'payresult.php');//支付回调地址
+define('GOLD_NOTICE', HTTP_SERVER . 'index.php?route=checkout/success');//支付成功跳转地址
