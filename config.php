0f7a0b9f634387764fb8315cc2ae0d568173e0f3
guhao <18810443670@163.com>
2018-03-29 14:40:13 +0800

    config

--- config.php
+++ config.php
@@ -39,6 +39,6 @@ define('GOLD_PAYURL', 'https://api.goldpayment.net/pay/');//支付请求的url
 
 define('GOLD_KEY', 'UVIXnJb7W4kpFevdDum5MHNBZSj06q39');//商户秘钥
 
-define('GOLD_NOTIFY', HTTP_SERVER.'notify.php');//支付回调地址
+define('GOLD_NOTIFY', HTTP_SERVER.'index.php?route=payment/notify');//支付回调地址
 
 define('GOLD_NOTICE', HTTP_SERVER.'payresult.php');//支付回调地址
