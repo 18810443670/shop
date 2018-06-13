ce935255892538925942dec374e94a9ba075ecac
guhao <18810443670@163.com>
2018-06-13 13:50:44 +0800

    footer

--- catalog/language/zh-cn/common/footer.php
+++ catalog/language/zh-cn/common/footer.php
@@ -8,20 +8,21 @@
  */
 
 // Text
-$_['text_information']  = '信息咨询';
-$_['text_service']      = '客户服务';
-$_['text_extra']        = '其他';
-$_['text_contact']      = '联系我们';
-$_['text_return']       = '退换服务';
-$_['text_sitemap']      = '网站地图';
+$_['text_information'] = '信息咨询';
+$_['text_service'] = '客户服务';
+$_['text_extra'] = '其他';
+$_['text_contact'] = '联系我们';
+$_['text_return'] = '退换服务';
+$_['text_sitemap'] = '网站地图';
 $_['text_manufacturer'] = '品牌专区';
-$_['text_voucher']      = '礼品券';
-$_['text_affiliate']    = '联盟会员';
-$_['text_special']      = '特别优惠';
-$_['text_account']      = '会员中心';
-$_['text_order']        = '历史订单';
-$_['text_wishlist']     = '收藏列表';
-$_['text_newsletter']   = '订阅咨询';
-$_['text_powered']      = '技术支持 <a href="http://www.opencart.cn">opencart.cn</a> %s &copy; %s';
-$_['text_layer_popup_title']      = '成功';
-$_['button_continue_shopping']    = '继续购物';
+$_['text_voucher'] = '礼品券';
+$_['text_affiliate'] = '联盟会员';
+$_['text_special'] = '特别优惠';
+$_['text_account'] = '会员中心';
+$_['text_order'] = '历史订单';
+$_['text_wishlist'] = '收藏列表';
+$_['text_newsletter'] = '订阅咨询';
+//$_['text_powered']      = '技术支持 <a href="http://www.opencart.cn">opencart.cn</a> %s &copy; %s';
+$_['text_powered'] = '';
+$_['text_layer_popup_title'] = '成功';
+$_['button_continue_shopping'] = '继续购物';
